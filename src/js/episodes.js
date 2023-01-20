e6c8227bcb66c3e8accc5d4bff5e75a88513099b
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-01-20 10:48:27 +0100

    new div; inner-section

--- src/js/episodes.js
+++ src/js/episodes.js
@@ -12,25 +12,30 @@ const fetchFirstPage = () => {
             for (let ep of episodes) {
                 output += `
                 <div class="character-card">
-                <div class="content-wrapper">
-                    <div class="sub-section">
-                        <span>Name:</span>
-                        <h2>${ep?.name}</h2>
-                    </div>
-                    <div class="sub-section">
-                        <span>Airdate:</span>
-                        <h2>${ep?.air_date}</h2>
-                    </div>
-                    <div class="sub-section">
-                        <span>Episode:</span>
-                        <h2>${ep?.episode}</h2>
-                    </div>
-                    <div class="sub-section">
-                        <span>Url:</span>
-                        <h2><a href="${ep?.url}">Link</a></h2>
+                    <div class="inner-section">
+                        <div class="image-wrapper">
+                            <img src="/Users/cosmasdaniella/Documents/Everything code/RickandMorty-verse/Rick--Morty-Season-6EWKSF-feature.avif"/>
+                        </div>
+                        <div class="content-wrapper">
+                            <div class="sub-section">
+                                <span>Name:</span>
+                                <h2>${ep?.name}</h2>
+                            </div>
+                            <div class="sub-section">
+                                <span>Airdate:</span>
+                                <h2>${ep?.air_date}</h2>
+                            </div>
+                            <div class="sub-section">
+                                <span>Episode:</span>
+                                <h2>${ep?.episode}</h2>
+                            </div>
+                            <div class="sub-section">
+                                <span>Url:</span>
+                                <h2><a href="${ep?.url}">Link</a></h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
             `
             }
         document.getElementById("charContainer").innerHTML = output;    
