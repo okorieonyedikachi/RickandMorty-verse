c105f38fae09b86d02b55e37f506f5cfd6baf886
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-01-27 21:47:38 +0100

    still battling with localstorage

--- src/js/detailCharacter.js
+++ src/js/detailCharacter.js
@@ -2,3 +2,6 @@
 // // console.log(myObjectString2);
 // const myObject2 = JSON.parse(myObjectString2)
 // console.log(myObject2);
+
+// const characterListString2 = localStorage.getItem('characters');
+// console.log(characterListString2);
