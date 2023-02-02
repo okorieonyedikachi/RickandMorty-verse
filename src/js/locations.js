a41c838ff691b1d901fdacfa1a832f5f7967a5e6
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-02-02 20:06:28 +0100

    fetching location api

--- src/js/locations.js
+++ src/js/locations.js
@@ -1,50 +1,14 @@
 const Loc_API = "https://rickandmortyapi.com/api/location"
-let locations; 
+// let locations; 
 
-// console.log(LOC_API);
-const fetchFirstPage = () => {
+// console.log(Loc_API);
+const fetchPage = () => {
     axios
         .get(Loc_API)
         .then(response => {
-            locations = response?.data
-            // console.log(locations);
-            let output = "";
-            for (let loc of locations) {
-                output += `
-                <div class="character-card">
-                <div class="inner-section">
-                    <div class="image-wrapper">
-                        <img src="../../images/Rick--Morty-Season-6EWKSF-feature.avif"/>
-                    </div>
-                    <div class="content-wrapper">
-                        <div class="sub-wrapper">
-                            <div class="sub-section">
-                                <span>Name:</span>
-                                <h2><a href="#">${loc?.name}</h2></a>
-                            </div>
-                            <div class="sub-section">
-                                <span>Type:</span>
-                                <h2><a href="#" rel="noopener">${loc?.type}</h2></a>
-                            </div>
-                            <div class="sub-section ">
-                                <span>Dimension:</span>
-                                <h2>${loc?.dimension}</h2>
-                            </div>
-                            <div class="sub-section sub3">
-                                <span>Residents:</span>
-                                <h2 id="c-list"><a href="${loc?.residents}">${loc?.residents?.length}</a></h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                `
-            }
-
-        document.getElementById("charContainer").innerHTML = output; 
-
+            locations = response?.data?.results
+            console.log(locations);
         })
-        .catch (error => console.error(error))
 }
 
-fetchFirstPage
+fetchPage()
