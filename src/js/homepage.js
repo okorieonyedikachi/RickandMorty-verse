5b6d6a9646285ee35ec62cff257ae7c585d1384e
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-02-11 23:37:14 +0100

    clean up

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -90,22 +90,3 @@ nextButton.addEventListener("click",() => {
     fetchSixCharacters()
 })
 
-
-// prevButton.addEventListener("click", function previousButton(
-//     url = "https://rickandmortyapi.com/api/character",
-//     page = 1,
-//     previousResponse = []) 
-// {
-//     return fetch(`${url}&page=${page}`)
-//     .then (response => response.json())
-//     .then(newResponse => {
-//         const response = [...previousResponse, ...newResponse];
-//         if (newResponse.length !== 0) {
-//             page++;
-    
-//             return paginated_fetch(url, page, response);
-//           }
-    
-//           return response;
-//     });
-// })
